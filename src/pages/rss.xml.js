6d701d7a1d47f579774b4eae6c35536d4c632018
milkmaccya2 <milkmaccya2@gmail.com>
2026-02-01 21:37:22 +0900

    Initial commit: Astro v6 blog setup

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,16 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+// import rss from '@astrojs/rss';
+// import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+    return new Response('RSS disabled temporarily due to Astro v6 compatibility.', { status: 404 });
+	// const posts = await getCollection('blog');
+	// return rss({
+	// 	title: SITE_TITLE,
+	// 	description: SITE_DESCRIPTION,
+	// 	site: context.site,
+	// 	items: posts.map((post) => ({
+	// 		...post.data,
+	// 		link: `/blog/${post.id}/`,
+	// 	})),
+	// });
 }
