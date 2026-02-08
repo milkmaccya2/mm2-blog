f77817138c6f5f5397f6a32e58fb03c0e52d6cd9
milkmaccya2 <milkmaccya2@gmail.com>
2026-02-08 12:25:26 +0900

    docs: reorder imports in astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
-import compress from 'astro-compress';
+
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
